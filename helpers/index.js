2a2568fd0de0478f22e16a63e981ece92c020e3b
Muhammad Jawahiruzzaman <m.jawahiruzzaman@gmail.com>
2023-01-29 12:59:30 +0700

    feat: add helpers

---
+++ helpers/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const secret_key = process.env.SECRET_KEY
+
+module.exports = {
+  hashPassword: (password) => bcrypt.hashSync(password, 12),
+  comparePassword: (password, hash) => bcrypt.compareSync(password, hash),
+  tokenEncode: (payload) => jwt.sign(payload, secret_key),
+  tokenDecode: (token) => jwt.verify(token, secret_key)
+}
