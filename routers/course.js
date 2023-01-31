b9d40734b94ebe7c20f5fa896688ef9b816de435
mitrasurya7 <62651166+mitrasurya7@users.noreply.github.com>
2023-01-31 07:55:02 +0700

    Delete routers directory

--- routers/course.js
+++
@@ -1,8 +0,0 @@
-const { forSearch } = require('../controllers/controllerCourse');
-const ControllerCourse = require('../controllers/controllerCourse');
-
-const router = require('express').Router();
-
-router.get('/?', ControllerCourse.twelevCourse);
-router.get('/search?', ControllerCourse.forSearch);
-module.exports = router;
