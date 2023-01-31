34b564ff4d3dff95cba3683e6ff56836713de74d
mitrasurya7 <62651166+mitrasurya7@users.noreply.github.com>
2023-01-31 07:54:33 +0700

    Delete helpers directory

--- helpers/bcrypt.js
+++
@@ -1,6 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-const encryptPass = (dataPass) => bcrypt.hashSync(dataPass, 10);
-const descryptPass = (dataPass, hashpass) => bcrypt.compareSync(dataPass, hashpass);
-
-module.exports = { encryptPass, descryptPass };
--- helpers/jwt.js
+++
@@ -1,7 +0,0 @@
-const jwt = require('jsonwebtoken');
-const kode = process.env.JWT_CODE;
-
-const encodeToken = (payload) => jwt.sign(payload, kode);
-const decodeToken = (token) => jwt.verify(token, kode);
-
-module.exports = { encodeToken, decodeToken };
