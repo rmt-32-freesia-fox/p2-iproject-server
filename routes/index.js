ccb3d2a96e9ec89236f6b27773c59f0609238aec
Muhammad Jawahiruzzaman <m.jawahiruzzaman@gmail.com>
2023-02-07 20:04:51 +0700

    feat: add food list

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router()
 const paymentRoute = require('./paymentRoute')
+const foodRoute = require('./foodRoute')
 const userRoute = require('./userRoute')
 const authenticated = require('../middlewware/authenticated')
 
 router.use(userRoute)
+router.use('/foods', foodRoute)
 
 router.use(authenticated)
 router.use(paymentRoute)
