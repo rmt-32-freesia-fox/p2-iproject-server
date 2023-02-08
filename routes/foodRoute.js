4f640ce5c626af2c715f4bedea6364a63c0b2a0b
Muhammad Jawahiruzzaman <m.jawahiruzzaman@gmail.com>
2023-02-08 09:25:20 +0700

    feat: get foods information, ingredient, and search by ingredients

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -3,5 +3,8 @@ const FoodController = require('../controllers/foodController')
 const router = require('express').Router()
 
 router.get('/', FoodController.foods)
+router.get('/ingredients', FoodController.ingredients)
+router.get('/:id', FoodController.informationFood)
+router.get('/findByIngredients', FoodController.findByIngredients)
 
 module.exports = router
