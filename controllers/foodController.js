83a4d7d6b7f5eb34c8256549219f1a2af15eba2a
Muhammad Jawahiruzzaman <m.jawahiruzzaman@gmail.com>
2023-02-08 09:25:20 +0700

    feat: get foods information, ingredient, and search by ingredients

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -14,6 +14,40 @@ class FoodController {
       next(error)
     }
   }
+
+  static async informationFood(req, res, next) {
+    try {
+      const { id } = req.params
+      const foods = await axios.get(`${HOST_SPOON}/recipes/${id}/information`, {
+        params: { includeNutrition: false, apiKey: API_KEY_SPOON }
+      })
+      res.status(200).json(foods.data)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  static async findByIngredients(req, res, next) {
+    try {
+      const foods = await axios.get(`${HOST_SPOON}/recipes/findByIngredients`, {
+        params: { ...req.query, apiKey: API_KEY_SPOON }
+      })
+      res.status(200).json(foods.data)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  static async ingredients(req, res, next) {
+    try {
+      const foods = await axios.get(`${HOST_SPOON}/food/ingredients/search`, {
+        params: { ...req.query, apiKey: API_KEY_SPOON }
+      })
+      res.status(200).json(foods.data)
+    } catch (error) {
+      next(error)
+    }
+  }
 }
 
 module.exports = FoodController
