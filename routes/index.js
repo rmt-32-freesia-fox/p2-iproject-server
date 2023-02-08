9cd05b03516806b2eaf8609d4dc3689578464f56
Muhammad Jawahiruzzaman <m.jawahiruzzaman@gmail.com>
2023-02-08 07:48:06 +0700

    feat: add payment gateway

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 const router = require('express').Router()
 const foodRoute = require('./foodRoute')
 const eventRoute = require('./eventRoute')
+const paymentRoute = require('./paymentRoute')
+const userRoute = require('./userRoute')
+const authenticated = require('../middlewware/authenticated')
 
 router.use(userRoute)
 router.use('/foods', foodRoute)
 router.use('/events', eventRoute)
+
+router.use(authenticated)
+router.use(paymentRoute)
 
 module.exports = router
