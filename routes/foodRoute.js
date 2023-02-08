4c128dc631422cb3578367c4a25d5f87ff745cfa
Muhammad Jawahiruzzaman <m.jawahiruzzaman@gmail.com>
2023-02-08 09:41:56 +0700

    fix: route

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -4,7 +4,7 @@ const router = require('express').Router()
 
 router.get('/', FoodController.foods)
 router.get('/ingredients', FoodController.ingredients)
-router.get('/:id', FoodController.informationFood)
 router.get('/findByIngredients', FoodController.findByIngredients)
+router.get('/:id', FoodController.informationFood)
 
 module.exports = router
