13ccc82873c09726ed240256800c9c04fe2de582
Muhammad Jawahiruzzaman <m.jawahiruzzaman@gmail.com>
2023-02-08 11:02:52 +0700

    feat: add route in root

--- routes/eventRoute.js
+++ routes/eventRoute.js
@@ -1,10 +1,13 @@
 const EventController = require('../controllers/eventController')
+const authenticated = require('../middlewware/authenticated')
 
 const router = require('express').Router()
 
 router.get('/', EventController.getEvents)
-router.post('/', EventController.addEvent)
 router.get('/:id', EventController.getById)
+
+router.use(authenticated)
+router.post('/', EventController.addEvent)
 router.put('/:id', EventController.replaceEvents)
 router.patch('/:id', EventController.updateStatus)
 router.delete('/:id', EventController.deleteEvent)
--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const paymentRoute = require('./paymentRoute')
 const foodRoute = require('./foodRoute')
 const userRoute = require('./userRoute')
 const authenticated = require('../middlewware/authenticated')
+const eventRoute = require('./eventRoute')
 
 router.use(userRoute)
 router.use('/foods', foodRoute)
+router.use('/events', eventRoute)
 
 router.use(authenticated)
 router.use(paymentRoute)
