ee63c466ad7ab1a36d4db1c48518c81dd8663551
mmacnesa <mmacnesa@gmail.com>
2023-02-08 14:39:23 +0700

    feat: add env files

--- app.js
+++ app.js
@@ -2,39 +2,38 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const midtransClient = require('midtrans-client');
+
 const cors = require('cors')
+app.use(cors())
 
 const axios = require('axios')
 
-const baseUrl = 'http://localhost:3000'
-
-app.use(cors())
 app.use(express.json())
-
 app.use(express.urlencoded({ extended: true }))
 
-const midtransClient = require('midtrans-client');
 
 const {User} = require('./models')
 
-var client_id = '29694d93e1d24518aec34551ed349c5e'; // Your client id
-var client_secret = 'bb6596f931934078b30f993bb36ab839'; // Your secret
-var redirect_uri = 'http://localhost:3000/redirect/'; // Your redirect uri
+const baseUrl = 'http://localhost:3000'
+// var client_id = '29694d93e1d24518aec34551ed349c5e'; // Your client id
+// var client_secret = 'bb6596f931934078b30f993bb36ab839'; // Your secret
+// var redirect_uri = 'http://localhost:3000/redirect/'; // Your redirect uri
 
-var myCode = 'AQC_TIB-Twa01xZMXvBalPtBe1plZaUMjmC7qnjGLzor7u68exboycNJlIzTs6-wSRqIw4xsRQc_0I2P6FHVdVDMHmXsKd-OMbmJiJvIEtCMUmEV2DJPi3bN7SB-ctYHx23rlin-rkuVVGmoYmcNe2QHRd9MpZBPGyS8gGRxsO5Xw62sauwh9-kcyvWM1R2izdZDehu3sHQBQ7NEb3S8wZLOtePKqCLwRnVc1fhmQkBiTkes3NUyi2spKUsjlsQxTiVkIhpN6AZmuA'
+// var myCode = 'AQC_TIB-Twa01xZMXvBalPtBe1plZaUMjmC7qnjGLzor7u68exboycNJlIzTs6-wSRqIw4xsRQc_0I2P6FHVdVDMHmXsKd-OMbmJiJvIEtCMUmEV2DJPi3bN7SB-ctYHx23rlin-rkuVVGmoYmcNe2QHRd9MpZBPGyS8gGRxsO5Xw62sauwh9-kcyvWM1R2izdZDehu3sHQBQ7NEb3S8wZLOtePKqCLwRnVc1fhmQkBiTkes3NUyi2spKUsjlsQxTiVkIhpN6AZmuA'
 
-var request = 'https://accounts.spotify.com/authorize?client_id=29694d93e1d24518aec34551ed349c5e&response_type=code&redirect_uri=http://localhost:3000/redirect/&scope=user-top-read%20user-read-recently-played%20user-read-currently-playing'
+// var request = 'https://accounts.spotify.com/authorize?client_id=29694d93e1d24518aec34551ed349c5e&response_type=code&redirect_uri=http://localhost:3000/redirect/&scope=user-top-read%20user-read-recently-played%20user-read-currently-playing'
 
-var request_token = 'https://accounts.spotify.com/api/token'
+// var request_token = 'https://accounts.spotify.com/api/token'
 
-var access_token = "BQAg-qaRNc7G2ZNxaxlQ1xzjNdgr5ELWJGbBf0-7QF_u7gLGGFFZHdVVxQyxx6xXj3gMnLIyHiGvU9CasZU9y9hujdyMjmq5V23LbpN2Uw6i_rtqOXrb9xVscb2VZWAyjdSA1K3_d6huvCoGDka7xdQhJ01HUXyRfbID0bwcsfX6eMbMoQpAxbxrwlZi6QnWLOxQCjB5cv7nakLO9Y1TlKHZ"
+// var access_token = "BQAg-qaRNc7G2ZNxaxlQ1xzjNdgr5ELWJGbBf0-7QF_u7gLGGFFZHdVVxQyxx6xXj3gMnLIyHiGvU9CasZU9y9hujdyMjmq5V23LbpN2Uw6i_rtqOXrb9xVscb2VZWAyjdSA1K3_d6huvCoGDka7xdQhJ01HUXyRfbID0bwcsfX6eMbMoQpAxbxrwlZi6QnWLOxQCjB5cv7nakLO9Y1TlKHZ"
 
-var server_key = "SB-Mid-server-LLsj1r0T21cb2IQx_o6shDCS"
+// var server_key = "SB-Mid-server-LLsj1r0T21cb2IQx_o6shDCS"
 
 const  buffer = 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
 // Basic Mjk2OTRkOTNlMWQyNDUxOGFlYzM0NTUxZWQzNDljNWU6YmI2NTk2ZjkzMTkzNDA3OGIzMGY5OTNiYjM2YWI4Mzk=
 
-
+const client_redirect_url = 'http://localhost:5173'
 
 
 
@@ -65,7 +64,7 @@ class Controller {
       // res.status(status).json({access_token})  
       
       // console.log(token.data.access_token);
-      res.redirect('http://localhost:5173/?token=' + data) 
+      res.redirect(client_redirect_url + '/?token=' + data) 
     } catch (error) {
       console.log(error, 'error nich');
     }
