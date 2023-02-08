093d893c7594a0f8b7a19ce534dcc54354be2889
Rhazzid Miftakhul Auzzura <rhazzid2@gmail.com>
2023-02-09 03:12:08 +0700

    refactor

--- routers/exercise.js
+++ routers/exercise.js
@@ -1,10 +1,8 @@
 const router = require("express").Router();
 const exerciseController = require("../controllers/exerciseController");
-const authentication = require("../middleware/authentication");
 
 // third api
 router.get("/muscle", exerciseController.getTargetMuscle);
-router.use(authentication);
 router.get("/bodyParts", exerciseController.getBodyParts);
 
 // rest api
