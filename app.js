0b5496c98f212776a8c46708100702388c43ddc9
Muhammad Jawahiruzzaman <m.jawahiruzzaman@gmail.com>
2023-02-09 08:43:22 +0700

    feat: add env example and setup to deploy

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+if (process.env.NODE_ENV != 'production') {
+  require('dotenv').config()
+}
+
 const express = require('express')
 const app = express()
-const port = 3000
 const cors = require('cors')
 const routes = require('./routes')
 const errorHandling = require('./middlewware/errorHandling')
+const port = process.env.PORT || 3000
+
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
