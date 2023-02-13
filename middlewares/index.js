557b8e57360bba39f4cf5e64e286fcb2ba5aca53
mmacnesa <mmacnesa@gmail.com>
2023-02-13 17:54:29 +0700

    middlewares shrinked

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,75 +1,48 @@
 const { Controller } = require('../controllers');
-const {User} = require('../models') 
+const { User } = require('../models')
 
-function errorHandler (error, req, res, next)  {
-//   {
-//     "error": {
-//         "status": 401,
-//         "message": "The access token expired"
-//     }
-// }
-let {code, name, errors, response} = error
+function errorHandler(error, req, res, next) {
+  let { code, name, errors, response } = error
 
-let spotifyError = {} 
-let number =  500
-let msg = 'Internal server error' 
-  if(response) {
-    if(response?.data?.error?.status) {
+  let number = 500
+  let message = 'Internal server error'
+
+  if (response) {
+    if (response?.data?.error?.status) {
       number = response?.data?.error?.status
     }
-    msg = response?.data?.error?.message
-  } 
-  // const {response: { data: { error:  { status, message} } } } = error
-  
-// console.log(error);
-
-  
-
-// let message = 'Internal server error' 
-// let number = code || 500
-
-  // switch(name) {
-  //   case 'SequelizeValidationError': 
-  //   case 'SequelizeUniqueConstraintError': 
-  //     message = {error : errors.map(each => each.message).join(', ')}
-  //     number = 400
-  //     break;
-  // }
-
-  switch(code) {
-    case 401:  
-      msg = name || 'Unrecognized identity'
-      break;
-    case 403: 
-      msg = name || 'Forbidden access'
-      break;
-    case 404: 
-      msg = name || 'Data not found'
-      break;
+    message = response?.data?.error?.message
   }
 
-  // res.status(number).json(error)
-  res.status(number).json({message: msg}) 
-} 
+  switch (code) {
+    case 401:
+      message = name || 'Unrecognized identity'
+      break;
+    case 403:
+      message = name || 'Forbidden access'
+      break;
+    case 404:
+      message = name || 'Data not found'
+      break;
+  }
+  res.status(number).json({ message })
+}
 
-async function auth (req, res, next) {
+async function auth(req, res, next) {
   try {
     const { access_token } = req.headers
-    if(!access_token) throw {code: 401}
-    
-    const { id } =  await Controller.getProfile(access_token)
-        
+    if (!access_token) throw { code: 401 }
+
+    const { id } = await Controller.getProfile(access_token)
+
     const findUser = await User.findOne({
       where: {
         userId: id
       }
     })
-    if(!findUser) throw {code: 401}  
+    if (!findUser) throw { code: 401 }
+    req.token = access_token
 
-    
-    
-    req.token = access_token  
-    // req.userRole = findUser.role
     next()
   } catch (error) {
     next(error)
